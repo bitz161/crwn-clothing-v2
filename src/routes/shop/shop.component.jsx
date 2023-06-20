7c9b4119a2f3fc5f96e4a197c4aa0305e01d628d
bitz161 <bitzgarcia1996@gmail.com>
2023-06-21 01:42:08 +0800

    product card

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,17 +1,15 @@
 import { useContext } from "react";
 import "./shop.styles.scss";
 import { ProductContext } from "../../context/products.context";
+import ProductCard from "../../components/product-card/product-card.component";
 
 const Shop = () => {
   const { products } = useContext(ProductContext);
   return (
-    <div>
-      {products.map(({ id, name, imageUrl, price }) => {
-        return (
-          <div key={id}>
-            <h1>{name}</h1>
-          </div>
-        );
+    <div className="products-container">
+      {products.map((product) => {
+        const { id } = product;
+        return <ProductCard product={product} key={id} />;
       })}
     </div>
   );
